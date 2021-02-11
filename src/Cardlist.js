c3fb21879bfd7cbb9ba8956b918c1c96b5e1038d
Caleb Matteis <matteismusic@gmail.com>
2021-02-11 07:14:13 -0500

    Completed Cardlist.js

--- src/Cardlist.js
+++ src/Cardlist.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import Card from "./Card";
 
+const Cardlist = (props) => {
+    const cards = props.cards.map(cardId => (
+        <Card cardId={cardId} key={cardId} />
+    ));
+    return <div>{cards}</div>
+}
+
+export default Cardlist;
